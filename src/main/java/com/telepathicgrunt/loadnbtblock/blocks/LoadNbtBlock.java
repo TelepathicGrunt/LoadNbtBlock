9501ba5f08d1a443378784bd56e7b33f84d7df64
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-04-07 14:09:13 -0400

    simplified

--- src/main/java/com/telepathicgrunt/loadnbtblock/blocks/LoadNbtBlock.java
+++ src/main/java/com/telepathicgrunt/loadnbtblock/blocks/LoadNbtBlock.java
@@ -1,7 +1,5 @@
 package com.telepathicgrunt.loadnbtblock.blocks;
 
-import com.telepathicgrunt.loadnbtblock.mixins.MinecraftServerAccessor;
-import com.telepathicgrunt.loadnbtblock.mixins.StructureManagerAccessor;
 import com.telepathicgrunt.loadnbtblock.utils.StructureNbtDataFixer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.*;
@@ -9,14 +7,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.StructureBlockBlockEntity;
 import net.minecraft.block.enums.StructureBlockMode;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.NbtIo;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
-import net.minecraft.resource.Resource;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ServerTask;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.TranslatableText;
@@ -25,32 +16,18 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.WorldChunk;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class LoadNbtBlock extends Block {
 
     public LoadNbtBlock() {
         super(Settings.of(Material.METAL, MaterialColor.LIGHT_GRAY).requiresTool().strength(-1.0F, 3600000.0F).dropsNothing());
     }
-
-    // source: https://github.com/williambl/explosivessquared/blob/master/src/main/kotlin/com/williambl/explosivessquared/util/actions/MassBlockActionManager.kt
-    @FunctionalInterface
-    interface task<One, Two, Three> {
-        void apply(One one, Two two, Three three);
-    }
-    private final Map<Long, Pair<Integer, task<Chunk, World, Integer>>> chunkJobs = new HashMap<>();
 
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if(!(world instanceof ServerWorld) || hand == Hand.MAIN_HAND) return ActionResult.PASS;
@@ -74,95 +51,54 @@ public class LoadNbtBlock extends Block {
         int rowCount = (int) Math.max(Math.ceil(identifiers.size()) / columnCount, 1);
         int spacing = 48;
         BlockPos bounds = new BlockPos(spacing * (rowCount+2), spacing, spacing * columnCount);
-        chunkJobs.clear();
-
-        Identifier id = new Identifier(com.telepathicgrunt.loadnbtblock.LoadNbtBlock.MODID, "sections/section1.nbt");
-        Identifier id2 = new Identifier(com.telepathicgrunt.loadnbtblock.LoadNbtBlock.MODID, "sections/section2.nbt");
-        CompoundTag compoundTag;
-        CompoundTag compoundTag2;
-        try {
-            ResourceManager resourceManager = ((StructureManagerAccessor)((ServerWorld) world).getStructureManager()).lnbtb_getField_25189();
-            Resource resource = resourceManager.getResource(id);
-            compoundTag = NbtIo.readCompressed(resource.getInputStream());
-
-            resource = resourceManager.getResource(id2);
-            compoundTag2 = NbtIo.readCompressed(resource.getInputStream());
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return ActionResult.FAIL;
-        }
-        ListTag palette1 = compoundTag.getList("Palette", 10);
-        long[] blockStates1 = compoundTag.getLongArray("BlockStates");
-        ListTag palette2 = compoundTag2.getList("Palette", 10);
-        long[] blockStates2 = compoundTag2.getLongArray("BlockStates");
-
-        short zero = 0;
-        short nonEmptyBlockCount = 256;
 
         // Fill/clear area with structure void
-        task<Chunk, World, Integer> taskToRun = (chunkIn, worldIn, yPos) -> {
-            ChunkSection[] sections = chunkIn.getSectionArray();
-
-            ChunkSection newChunkSection = new ChunkSection(0, nonEmptyBlockCount, zero, zero);
-            newChunkSection.getContainer().read(palette1, blockStates1);
-            sections[0] = newChunkSection;
-
-            newChunkSection = new ChunkSection(1, nonEmptyBlockCount, zero, zero);
-            newChunkSection.getContainer().read(palette2, blockStates2);
-            sections[1] = newChunkSection;
-
-            newChunkSection = new ChunkSection(2, nonEmptyBlockCount, zero, zero);
-            newChunkSection.getContainer().read(palette2, blockStates2);
-            sections[2] = newChunkSection;
-        };
-
         BlockPos.Mutable mutableChunk = new BlockPos.Mutable().set(pos.getX() >> 4, pos.getY(), pos.getZ() >> 4);
         mutableChunk.move(1,0,0);
         int endChunkX = (pos.getX() + bounds.getX()) >> 4;
         int endChunkZ = (pos.getZ() + bounds.getZ()) >> 4;
 
+        int maxChunks = (endChunkX-mutableChunk.getX()) + (endChunkZ-mutableChunk.getZ());
+        int currentSection = 0;
+        BlockPos.Mutable mutablePos = new BlockPos.Mutable();
         for(; mutableChunk.getX() < endChunkX; mutableChunk.move(1,0,0)) {
             for (; mutableChunk.getZ() < endChunkZ; mutableChunk.move(0, 0, 1)) {
-                chunkJobs.computeIfAbsent(
-                        ChunkPos.toLong(mutableChunk.getX(), mutableChunk.getZ()), // Chunk to clear
-                        (chunkLong) -> Pair.of(mutableChunk.getY(), taskToRun) // task to run at y pos
-                );
+
+                WorldChunk chunk = world.getChunk(mutableChunk.getX(), mutableChunk.getZ());
+
+                for(int y = -1; y < 48; y++){
+                    int yOffset = pos.getY() + y;
+                    BlockState stateToUse;
+                    if(y == -1){
+                        stateToUse = Blocks.STONE.getDefaultState();
+                    }
+                    else if(y == 0){
+                        stateToUse = Blocks.BARRIER.getDefaultState();
+                    }
+                    else{
+                        stateToUse = Blocks.STRUCTURE_VOID.getDefaultState();
+                    }
+
+                    for(int x = 0; x < 16; x++){
+                        for(int z = 0; z < 16; z++){
+                            mutablePos.set(x, yOffset, z);
+                            chunk.setBlockState(mutablePos, stateToUse, false);
+                        }
+                    }
+                }
+
+                currentSection++;
+                chunk.markDirty();
+                // Send changes to client to see
+                ((ServerChunkManager) world.getChunkManager()).threadedAnvilChunkStorage
+                        .getPlayersWatchingChunk(chunk.getPos(), false)
+                        .forEach(s -> s.networkHandler.sendPacket(new ChunkDataS2CPacket(chunk, 65535)));
+                player.sendMessage(new TranslatableText("Working: %" +  Math.round(((float)currentSection / maxChunks) * 100f) / 100f), true);
             }
             mutableChunk.set(mutableChunk.getX(), mutableChunk.getY(), pos.getZ() >> 4); // Set back to start of row
         }
 
-        MinecraftServer executor = world.getServer();
-        AtomicInteger completedSections = new AtomicInteger();
-
-        chunkJobs.forEach((key, chunkJobCollection) ->
-            executor.execute(new ServerTask(((MinecraftServerAccessor)executor).lnbtb_getTicks(), () -> {
-                int chunkX = ChunkPos.getPackedX(key);
-                int chunkZ = ChunkPos.getPackedZ(key);
-                WorldChunk chunkToClear = world.getChunk(chunkX, chunkZ);
-
-                // Run the task to clear the chunk
-                chunkJobCollection.getRight().apply(chunkToClear, world, chunkJobCollection.getLeft());
-
-                // Send changes to client to see
-                chunkToClear.markDirty();
-                ((ServerChunkManager) world.getChunkManager()).threadedAnvilChunkStorage
-                        .getPlayersWatchingChunk(chunkToClear.getPos(), false)
-                        .forEach(s -> s.networkHandler.sendPacket(new ChunkDataS2CPacket(chunkToClear, 65535)));
-
-                // Tell player progress so they know it is working
-                int currentSection = completedSections.get() + 1;
-                completedSections.set(currentSection);
-                player.sendMessage(new TranslatableText("Working: %" + (((float)currentSection / chunkJobs.size()) * 100)), true);
-
-                // Places structure blocks and loads pieces when last task is completed
-                // TODO: make this be threaded as well.
-                if(currentSection == chunkJobs.size()){
-                    generateStructurePieces(world, pos, player, identifiers, columnCount, spacing, mutableChunk);
-                }
-            }))
-        );
-
+        generateStructurePieces(world, pos, player, identifiers, columnCount, spacing, mutableChunk);
         return ActionResult.SUCCESS;
     }
 
